bb54fb85566eccb964655a74ce0d52a5a4dc44e9
powers.nick <powers.nick@32cf6871-3d1b-0410-89cb-57f39ccbec16>
2006-08-18 14:25:15 +0000

    Added some functions.

--- trunk/thread_manager.c
+++ trunk/thread_manager.c
@@ -6,3 +6,38 @@
 #include <libgen.h>
 #include "multi_thread.h"
 #include "util.h"
+
+
+pthread_t *pool_to_manage = NULL;
+struct timespec wait_time;
+
+
+/******************************************************************************/
+/******************************************************************************/
+void create_thread_manager(pthread_t *thread_pool){
+    const char *METHOD_NM = "create_thread_manager: ";
+    int thread_id;
+
+    if(thread_pool == NULL){
+        printf("%s pool to manage is NULL!", METHOD_NM);
+        return;
+    }
+
+    pool_to_manage = thread_pool;
+    rc = pthread_create(&thread_id, NULL, begin_managing, (void *)arg);
+
+
+
+}
+
+/******************************************************************************/
+/******************************************************************************/
+void check_threads(){
+}
+
+/******************************************************************************/
+/* The default function the thread_manager executes.                          */
+/******************************************************************************/
+void *begin_managing(){
+
+}
