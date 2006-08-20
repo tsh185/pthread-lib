1351fb00daca0f5a3aaeeae8785ce3a7945fcdd9
powers.nick <powers.nick@32cf6871-3d1b-0410-89cb-57f39ccbec16>
2006-08-20 18:09:38 +0000

    added CHECK_STATUS macro.

--- trunk/util.h
+++ trunk/util.h
@@ -8,4 +8,8 @@
 #define FALSE !TRUE
 
 typedef int BOOL;
+
+#define CHECK_STATUS(x,y){if(x != 0){if(y){printf("%s",y);} printf(" (status = %d)\n", status); } }
+#define CHECK_STATUS_UNIX(status, api, msg){if( status != 0 ) {int _errno = errno;if( api )printf( "%s ", api );printf( "failure" );if( msg )printf( ": %s", msg );printf( " (status = %d, errno = %d)\n", status, _errno );exit( 1 ); }}
+
 #endif
