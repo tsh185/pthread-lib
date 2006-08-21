8b7bd87acddbe8dc4825c6de29b6fb06ac6a7d71
powers.nick <powers.nick@32cf6871-3d1b-0410-89cb-57f39ccbec16>
2006-08-21 21:24:25 +0000

    Many updates.

--- trunk/thread_manager.h
+++ trunk/thread_manager.h
@@ -2,4 +2,39 @@
 #define __THREAD_MANAGER__
 
 
+/* Configuration file names */
+#define TASK "task"
+
+#define INTERVAL "interval" /* any time in HH24:MM:SS */
+
+#define THREAD_ACTION "thread.action"
+#define KILL "kill.thread"
+#define REPLACE "replace"
+#define KILL_PROGRAM "kill.program"
+
+#define WORK_LOAD_INCREASE "work.load.increase"
+#define WORK_LOAD_DECREASE "work.load.decrease"
+#define ADD "add"
+#define SUB "sub"
+#define VALUE "value"
+
+#define ACTION "action"
+#define BOUNDS "bounds"
+#define GREATER ">"
+#define LESS "<"
+
+#define INTERNAL_CONFIG_FILENAME "thread_manager.config"
+#define READ "r"
+#define FILE_LINE_SIZE 180
+
+
+/* Public Methods */
+
+void create_thread_manager(pthread_t *thread_pool, int num_threads);
+int *check_threads(pthread_t *threads, int num_threads);
+void read_config_file(char *filename);
+
+print_thread_status(pthread_t *thread_pool, int num_threads);
+void set_ping_signal(int signal);
+
 #endif
