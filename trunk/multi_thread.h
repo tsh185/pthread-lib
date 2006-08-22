5c4b32be85a1df9eb45a882f5cd4c94c6dbdb356
powers.nick <powers.nick@32cf6871-3d1b-0410-89cb-57f39ccbec16>
2006-08-22 16:01:29 +0000

    Added function prototypes.

--- trunk/multi_thread.h
+++ trunk/multi_thread.h
@@ -1,6 +1,9 @@
 #ifndef __MULTI_THREAD_H__
 #define __MULTI_THREAD_H__
 
+#include <pthread.h>
+#include "util.h"
+
 /* Error Codes */
 #define ERROR_CODE_MALLOC 1
 #define ERROR_CODE_SIG_HANDLER_NO_FUNCTIONS 2
@@ -12,12 +15,19 @@ typedef void (*FUNC_PTR)();
 void create_threads(void *(*func_ptr)(), void *parameter, int num_threads);
 void join_threads(int *t_status);
 void check_status(int status, char *api, char *msg);
-int timed_wait(int wait_secs);
-int timed_wait_milli(int wait_secs);
+int  timed_wait(int wait_secs);
+int  timed_wait_milli(int wait_secs);
 void destroy_all_mutexes();
 
 void stop_threads();
-int should_stop();
+int  should_stop();
+
+BOOL set_status_element(int index, int status);
+int *get_status_array();
+void init_status_array();
+void create_status_array();
+int  block_most_signals();
+int  get_pool_size();
 
 
 struct function_ptrs_struct {
