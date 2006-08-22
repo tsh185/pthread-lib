bb8329576ca8bc06f61d33f93d55467ed6bf2944
powers.nick <powers.nick@32cf6871-3d1b-0410-89cb-57f39ccbec16>
2006-08-22 16:02:07 +0000

    Initial add.

---
+++ trunk/example1.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <libgen.h>
+#include "multi_thread.h"
+
+void *stop(){
+  stop_threads();
+}
+
+void *print_report(){
+  printf("Report 1\n");
+}
+
+void *do_something(){
+
+  /* have this thread block all the signals */
+  block_most_signals();
+
+  while(!should_stop()){
+    timed_wait(5);
+  }
+}
+
+int main(){
+  /* assign the stop function to the signal handler */
+  FUNCTION_PTRS f_ptrs;
+  f_ptrs.term_func_ptr = stop;
+  f_ptrs.user1_func_ptr = print_report;
+
+  /* create the signal handler */
+  signal_handler_create(&f_ptrs);
+
+  create_threads(do_something, NULL, 5);
+
+  while(!should_stop()){
+    timed_wait(2);
+  }
+
+  join_threads(NULL);
+
+}
