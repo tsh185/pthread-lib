a4bef7b6433d76d527ae41f6802ea059a013b283
powers.nick <powers.nick@32cf6871-3d1b-0410-89cb-57f39ccbec16>
2006-08-22 21:40:42 +0000

    Added license header.

--- trunk/multi_thread.h
+++ trunk/multi_thread.h
@@ -1,3 +1,23 @@
+/*
+    pthread-lib is a set of pthread wrappers with additional features.
+    Copyright (C) 2006  Nick Powers
+    See <http://code.google.com/p/pthread-lib/> for more details and source.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef __MULTI_THREAD_H__
 #define __MULTI_THREAD_H__
 
@@ -12,7 +32,7 @@
 typedef void (*FUNC_PTR)();
 
 /* Thread Operations */
-void create_threads(void *(*func_ptr)(), void *parameter, int num_threads);
+pthread_t *create_threads(void *(*func_ptr)(), void *parameter, int num_threads);
 void join_threads(int *t_status);
 void check_status(int status, char *api, char *msg);
 int  timed_wait(int wait_secs);
