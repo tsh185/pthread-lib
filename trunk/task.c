bd2f476c1c55d2be2885b759ee13d4d4ee3cef5e
powers.nick <powers.nick@32cf6871-3d1b-0410-89cb-57f39ccbec16>
2006-08-23 18:12:28 +0000

    Updates to fix compilation problems.

--- trunk/task.c
+++ trunk/task.c
@@ -22,17 +22,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include <errno.h>
 #include <libgen.h>
-#include "util.h"
 #include "task.h"
 
-
-TASK *tasks = NULL;
+/* Global Variables */
+TASK_T *tasks = NULL;
 int capacity = 0;
-TASK *next_empty_index = NULL;
+int next_empty_index = NULL;
 int size;
 
+/* Private Functions */
+void _copy_tm(struct tm *dest_time, struct tm *src_time);
+void _copy_task(TASK_T *dest_task, TASK_T *src_task);
+void _resize_tasks();
+int _get_time_from_string(char *string);
+int _get_task_by_id(int id);
 
 /*****************************************************************************/
 /*! @fn void init_task(int num_tasks);
@@ -43,16 +49,16 @@ int size;
 */
 /*****************************************************************************/
 void init_tasks(int num_tasks){
-  const char *METHOD_NM = "init_task: ";
+  const char *METHOD_NM = "init_tasks: ";
 
   if(num_tasks <= 0){
     return;
   }
 
-  tasks = (TASK *)malloc(sizeof(TASK)*num_tasks);
+  tasks = (TASK_T *)malloc(sizeof(TASK_T)*num_tasks);
   CHECK_MALLOC(tasks,METHOD_NM,"malloc of tasks failed!");
 
-  memset(tasks, 0, sizeof(TASK)*capacity);
+  memset(tasks, 0, sizeof(TASK_T)*capacity);
   capacity = num_tasks;
   next_empty_index = 0;
   size = 0;
@@ -66,15 +72,53 @@ void init_tasks(int num_tasks){
 */
 /*****************************************************************************/
 void init_tasks_default(){
-  init_task(INITIAL_NUM_TASKS);
+  init_tasks(INITIAL_NUM_TASK_TS);
 }
 
 /*****************************************************************************/
-/*! @fn BOOL add_task(TASK *task)
+/*! @fn TASK_T *get_init_task()
+    @brief Returns a initialized task (using malloc)
+
+    Creates a dynamic piece of memory holding an initalized TASK_T.
+*/
+/*****************************************************************************/
+TASK_T *get_init_task(){
+  const char *METHOD_NM = "get_init_task: ";
+
+  TASK_T *task = (TASK_T *)malloc(sizeof(TASK_T));
+  CHECK_MALLOC(task, METHOD_NM, "malloc of task failed!");
+
+  memset(task, 0, sizeof(TASK_T));
+
+  return task; 
+}
+
+/*****************************************************************************/
+/*! @fn void destroy_task(TASK_T *task)
+    @brief Frees the allocated memory
+
+    Does a simple FREE task.
+*/
+/*****************************************************************************/
+void destroy_task(TASK_T *task){
+  FREE(task);
+}
+
+/*****************************************************************************/
+/*! @fn void destroy_all_tasks()
+    @brief Fress the \a tasks variable that holds all the tasks
+*/
+/*****************************************************************************/
+void destroy_all_tasks(){
+  destroy_task(tasks);
+}
+
+/*****************************************************************************/
+/*! @fn BOOL add_task(TASK_T *task)
     @brief Copies the task from source into the next available spot
 */
 /*****************************************************************************/
-BOOL add_task(TASK *task){
+BOOL add_task(TASK_T *task){
   if(task == NULL){
     return FALSE;
   }
@@ -93,11 +137,13 @@ BOOL add_task(TASK *task){
 /*****************************************************************************/
 /*****************************************************************************/
 BOOL add_tasks_by_file(char *filename){
+  /*
   fopen(filename,READ);
 
 
 
   fclose(filename);
+  */
 
 }
 
@@ -111,7 +157,7 @@ BOOL remove_task(){
     return FALSE;
   }
 
-  memset(tasks[--next_empty_index],0,sizeof(TASK));
+  memset(&tasks[--next_empty_index],0,sizeof(TASK_T));
 
   return TRUE;
 }
@@ -139,13 +185,13 @@ void set_id_byid(int id, int new_id){
 }
 
 /*****************************************************************************/
-/*! @fn void set_id(TASK *task, int new_id)
+/*! @fn void set_id(TASK_T *task, int new_id)
     @brief Sets the id of the task
-    @param TASK *task Task to be updated with a new id
+    @param TASK_T *task Task to be updated with a new id
     @param new_id New id of the task
 */
 /*****************************************************************************/
-void set_id(TASK *task , int new_id){
+void set_id(TASK_T *task , int new_id){
   task->id = new_id;
 }
 
@@ -168,13 +214,13 @@ void set_interval_byid(int id, char *interval){
 }
 
 /*****************************************************************************/
-/*! @fn void set_interval(TASK *task, char *interval)
+/*! @fn void set_interval(TASK_T *task, char *interval)
     @brief Sets the \a interval and \a _interval of the task
-    @param TASK *task The task to be changed
+    @param TASK_T *task The task to be changed
     @param char *interval The interval in HH:MM:SS form
 */
 /*****************************************************************************/
-void set_interval_byid(TASK *task, char *interval){
+void set_interval(TASK_T *task, char *interval){
   if(interval != NULL){
     return;
   }
@@ -202,9 +248,9 @@ void set_dead_thread_action_byid(int id, thread_action action){
 }
 
 /*****************************************************************************/
-/*! @fn void set_dead_thread_action(TASK *task, thread_action action)
+/*! @fn void set_dead_thread_action(TASK_T *task, thread_action action)
     @brief Sets the dead_thread_action of the task
-    @param TASK *task Task to be changed
+    @param TASK_T *task Task to be changed
     @param thread_action action Action to be changed (it's an enum)
 
     Options for thread_action are:
@@ -213,7 +259,7 @@ void set_dead_thread_action_byid(int id, thread_action action){
     - kill_program
 */
 /*****************************************************************************/
-void set_dead_thread_action(TASK *task, thread_action action){
+void set_dead_thread_action(TASK_T *task, thread_action action){
   task->dead_thread_action = action;
 }
 
@@ -244,9 +290,9 @@ void set_work_load_action_byid(int id, work_load_type type, work_load_action act
 
 
 /*****************************************************************************/
-/*! @fn void set_work_load_action(TASK *task, work_load_type type, work_load_action action)
+/*! @fn void set_work_load_action(TASK_T *task, work_load_type type, work_load_action action)
     @brief Sets the action of a work load
-    @param TASK *task Task to be changed
+    @param TASK_T *task Task to be changed
     @param work_load_type type Increase or decrease
     @param work_load_action action Action to be assinged to this work_load
 
@@ -259,7 +305,7 @@ void set_work_load_action_byid(int id, work_load_type type, work_load_action act
     - sub
 */
 /*****************************************************************************/
-void set_work_load_action(TASK *task, work_load_type type, work_load_action action){
+void set_work_load_action(TASK_T *task, work_load_type type, work_load_action action){
   if(type == increase){
     task->load_increase.action = action;
   } else if(type == decrease){
@@ -296,9 +342,9 @@ void set_work_load_upper_bound_byid(int id, work_load_type type, int bound){
 
 
 /*****************************************************************************/
-/*! @fn void set_work_load_upper_bound(TASK *task, work_load_type type, int bound
+/*! @fn void set_work_load_upper_bound(TASK_T *task, work_load_type type, int bound
     @brief Sets the upper bound of the work_load with an int
-    @param TASK *task Task to be changed
+    @param TASK_T *task Task to be changed
     @param work_load_type type Increase or decrease
     @param int bound Bound to assingn the task
 
@@ -309,7 +355,7 @@ void set_work_load_upper_bound_byid(int id, work_load_type type, int bound){
     valid bounds are any positive integer
 */
 /*****************************************************************************/
-void set_work_load_upper_bound(TASK *task, work_load_type type, int bound){
+void set_work_load_upper_bound(TASK_T *task, work_load_type type, int bound){
   if(bound <= 0){
     return;
   }
@@ -349,9 +395,9 @@ void set_work_load_lower_bound_byid(int id, work_load_type type, int bound){
 }
 
 /*****************************************************************************/
-/*! @fn void set_work_load_lower_bound(TASK *task, work_load_type type, int bound)
+/*! @fn void set_work_load_lower_bound(TASK_T *task, work_load_type type, int bound)
     @brief Sets the lower bound of the work_load with an int
-    @param TASK *task Task to be changed
+    @param TASK_T *task Task to be changed
     @param work_load_type type Increase or decrease
     @param int bound Bound to assingn the task
 
@@ -362,7 +408,7 @@ void set_work_load_lower_bound_byid(int id, work_load_type type, int bound){
     valid bounds are any negative integer
 */
 /*****************************************************************************/
-void set_work_load_lower_bound(TASK *task, work_load_type type, int bound){
+void set_work_load_lower_bound(TASK_T *task, work_load_type type, int bound){
   if(bound > 0){
     return;
   }
@@ -375,13 +421,60 @@ void set_work_load_lower_bound(TASK *task, work_load_type type, int bound){
 }
 
 /*****************************************************************************/
+/*! @fn void set_work_load_value_byid(int id, work_load_type, int value)
+    @brief Sets the value part of the work_load
+    @param int id Id of the task to be changed
+    @param work_load_type type Increase or decrease
+    @param int value Value to increase or decrease the threads
+
+    Value must be a positive integer.
+
+*/
+/*****************************************************************************/
+void set_work_load_value_byid(int id, work_load_type type, int value){
+  if(value <= 0){
+    return;
+  }
+
+  int index = _get_task_by_id(id);
+  if(type == increase){
+    tasks[index].load_increase.value = value;
+  } else if(type == decrease){
+    tasks[index].load_decrease.value = value;
+  }
+}
+
+/*****************************************************************************/
+/*! @fn void set_work_load_value(TASK_T *task, work_load_type type, int value)
+    @brief Sets the value part of the work_load
+    @param TASK_T *task Task to be changed
+    @param work_load_type type Increase or decrease
+    @param int value Value to increase or decrease the threads
+
+    Value must be a positive integer.
+
+*/
+/*****************************************************************************/
+void set_work_load_value(TASK_T *task, work_load_type type, int value){
+  if(value <= 0){
+    return;
+  }
+
+  if(type == increase){
+    task->load_increase.value = value;
+  } else if(type == decrease){
+    task->load_decrease.value = value;
+  }
+}
+
+/*****************************************************************************/
 /*! @fn void set_function_ptr_byid(int id, FUNC_PTR ptr)
     @brief Sets the function pointer to execute when this task is triggered
     @param int id Id of the task to be changed
     @param FUNC_PTR ptr Pointer to a function
 */
 /*****************************************************************************/
-void set_function_ptr_byid(int id, FUNC_PTR ptr){
+void set_function_ptr_byid(int id, void *(*ptr)() ){
   if(ptr == NULL){
     return;
   }
@@ -391,13 +484,13 @@ void set_function_ptr_byid(int id, FUNC_PTR ptr){
 }
 
 /*****************************************************************************/
-/*! @fn void set_function_ptr(TASK *task, FUNC_PTR ptr)
+/*! @fn void set_function_ptr(TASK_T *task, FUNC_PTR ptr)
     @brief Sets the function pointer to execute when this task is triggered
-    @param TASK *task Task to be changed
+    @param TASK_T *task Task to be changed
     @param FUNC_PTR ptr Pointer to a function
 */
 /*****************************************************************************/
-void set_function_ptr_byid(TASK *task, FUNC_PTR ptr){
+void set_function_ptr(TASK_T *task, void *(*ptr)() ){
   if(ptr == NULL){
     return;
   }
@@ -406,21 +499,83 @@ void set_function_ptr_byid(TASK *task, FUNC_PTR ptr){
 }
 
 /*****************************************************************************/
+/*! @fn void set_schedule_byid(int id, struct tm *time, BOOL repeat)
+    @brief Sets the schedule of the task
+    @param int id Id of the task to be changed
+    @param struct tm *time Time that the task will start
+    @param BOOL repeat Will task repeat?
+
+*/
+/*****************************************************************************/
+void set_schedule_byid(int id, struct tm *time, BOOL repeat){
+
+  int index = _get_task_by_id(id);
+  _copy_tm(&(tasks[index].schedule.start), time);
+  tasks[index].schedule.repeat = repeat;
+}
+
+/*****************************************************************************/
+/*! @fn void set_schedule(TASK_T *task, struct tm *time, BOOL repeat)
+    @brief Sets the schedule of the task
+    @param TASK_T *task Task to be changed
+    @param struct tm *time Time the task will start
+    @param BOOL repeat Will task repeat?
+
+*/
+/*****************************************************************************/
+void set_schedule(TASK_T *task, struct tm *time, BOOL repeat){
+  _copy_tm(&(task->schedule.start),time);
+  task->schedule.repeat = repeat;
+}
+
+/*****************************************************************************/
+/*! @fn int get_size()
+    @brief Returns the size of the \a tasks
+*/
+/*****************************************************************************/
+int get_size(){
+  return size;
+}
+
+/*****************************************************************************/
+/*! @fn int get_capacity()
+    @brief Returns the capacity of the \a tasks
+*/
+/*****************************************************************************/
+int get_capacity(){
+  return capacity;
+}
+
+/*****************************************************************************/
 /*                        Private Functions                                  */
 /*****************************************************************************/
 
 /*****************************************************************************/
-/*! @fn  _copy_task(TASK *dest_task, TASK *src_task)
+/*! @fn  _copy_task(TASK_T *dest_task, TASK_T *src_task)
     @brief Initailizes dest_task, then copies src_dest to dest_task
-    @param TASK *dest_task destination task
-    @param TASK *src_task source task
+    @param TASK_T *dest_task Destination task
+    @param TASK_T *src_task  Source task
 
     A full memory copy from src to dest.
 */
 /*****************************************************************************/
-void _copy_task(TASK *dest_task, TASK *src_task){
-  memset(dest_task,0,sizeof(TASK));
-  memcpy(dest_task,src_task,sizeof(TASK));
+void _copy_task(TASK_T *dest_task, TASK_T *src_task){
+  memset(dest_task,0,sizeof(TASK_T));
+  memcpy(dest_task,src_task,sizeof(TASK_T));
+}
+
+/*****************************************************************************/
+/*! @fn void _copy_tm(struct tm *dest_time, struct tm *src_time)
+    @brief Initializes dest_time, then copies src_time to dest_time
+    @param struct tm *dest_time Destination time
+    @param struct tm *src_time  Source time
+
+    A full memory copy from src to dest
+*/
+/*****************************************************************************/
+void _copy_tm(struct tm *dest_time, struct tm *src_time){
+  memset(dest_time,0,sizeof(struct tm));
+  memcpy(dest_time,src_time,sizeof(struct tm));
 }
 
 /*****************************************************************************/
@@ -430,9 +585,9 @@ void _copy_task(TASK *dest_task, TASK *src_task){
 /*****************************************************************************/
 void _resize_tasks(){
 
-  int new_size = capacity + NUM_TASKS_INCREMENT;
+  int new_size = capacity + NUM_TASK_TS_INCREMENT;
   int i;
-  TASK *t = (TASK *)malloc(sizeof(TASK)*new_size);
+  TASK_T *t = (TASK_T *)malloc(sizeof(TASK_T)*new_size);
 
   /* copy tasks in original */
   for(i=0; i<capacity; i++){
@@ -457,19 +612,21 @@ int _get_time_from_string(char *string){
   int min = 0;
   int sec = 0;
   char *sp = NULL;
-  char *str = NULL;
+  char *str = NULL; 
+  const char *delim = ":";
+
 
   if(string == NULL || strlen(string) > TIME_LEN){
     return -1;
   }
 
-  str=strtok_r(string,":",&sp);
+  str = strtok_r(string, delim, &sp);
   if(str) hour = atoi(str);
 
-  str=strtok_r(NULL,":", &sp);
+  str = strtok_r(NULL, delim, &sp);
   if(str) min = atoi(str);
 
-  str=strtok_r(NULL,":", &sp);
+  str = strtok_r(NULL, delim, &sp);
   if(str) sec = atoi(str);
 
   return ((hour*60)+min);
@@ -484,10 +641,50 @@ int _get_time_from_string(char *string){
 int _get_task_by_id(int id){
   int i;
   for(i=0; i<size; i++){
-    if(tasks[i]->id == id){
+    if(tasks[i].id == id){
       return i;
     }
   }
 
   return -1;
 }
+
+/*! This public-domain C implementation by Darel R. Finley.
+
+  - Returns true if sort was successful, or false if the nested
+    pivots went too deep, in which case your array will have
+    been re-ordered, but probably not sorted correctly.
+
+  - This function assumes it is called with valid parameters.
+
+  - Example calls:
+    quickSort(&myArray[0],5); // sorts elements 0, 1, 2, 3, and 4
+    quickSort(&myArray[3],5); // sorts elements 3, 4, 5, 6, and 7
+
+  - This function was modified by Nick Powers on 2006/08/22.
+*/
+int quick_sort_tasks(TASK_T *arr, int elements) {
+
+  int  piv, beg[Q_SORT_MAX_LEVELS], end[Q_SORT_MAX_LEVELS], i=0, L, R;
+
+  beg[0]=0; end[0]=elements;
+  while (i>=0) {
+    L=beg[i];
+    R=end[i]-1;
+
+    if (L<R) {
+      piv=arr[L]._interval;
+      if (i==Q_SORT_MAX_LEVELS-1) return FALSE;
+      while (L<R) {
+        while (arr[R]._interval >=piv && L<R) R--; if (L<R) arr[L++]=arr[R];
+        while (arr[L]._interval <=piv && L<R) L++; if (L<R) arr[R--]=arr[L];
+      }/* end while */
+      arr[L]._interval=piv; beg[i+1]=L+1; end[i+1]=end[i]; end[i++]=L;
+    }
+    else {
+      i--;
+    }/* end else */
+  }/* end while */
+  return TRUE;
+}
+
